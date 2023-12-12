317ba8de5fefa8bcd9a933a8c58ee9cb2859ed05
tanyareuben <tanyareuben@gmail.com>
2023-12-12 00:49:35 -0800

    Added all the cases accordingly except for anything Btype
    - BEQ
    - BNE
    - BLT
    - BGE
    - BLTU

--- src/RTypeInstruction.java
+++ src/RTypeInstruction.java
@@ -23,12 +23,61 @@ public class RTypeInstruction extends Instruction {
 		        registers.writeToRegister(rd, resultADD);
 		        break;
 		        
-	        
-//	TODO
-//            case InstructionSet.SUB:
-//                // Actual logic for SUB operation
-//                // Example: register[rd] = register[rs1] - register[rs2];
-//                break;
+	        case InstructionSet.SUB:
+	            int resultSUB = registers.getRegister(rs1) - registers.getRegister(rs2);
+	            registers.writeToRegister(rd, resultSUB);
+	            break;
+
+	        case InstructionSet.SLL:
+	            int shiftAmount = registers.getRegister(rs2) & 0x1F; // Extract lower 5 bits
+	            int resultSLL = registers.getRegister(rs1) << shiftAmount;
+	            registers.writeToRegister(rd, resultSLL);
+	            break;
+	            
+	        case InstructionSet.SLT:
+	            int resultSLT = (registers.getRegister(rs1) < registers.getRegister(rs2)) ? 1 : 0;
+	            registers.writeToRegister(rd, resultSLT);
+	            break;
+	            
+	        case InstructionSet.SLTU:
+	            int resultSLTU = (Integer.compareUnsigned(registers.getRegister(rs1), registers.getRegister(rs2)) < 0) ? 1 : 0;
+	            registers.writeToRegister(rd, resultSLTU);
+	            break;
+	            
+	        case InstructionSet.XOR:
+	            int resultXOR = registers.getRegister(rs1) ^ registers.getRegister(rs2);
+	            registers.writeToRegister(rd, resultXOR);
+	            break;
+	            
+	        case InstructionSet.SRL:
+	            int shiftValue = (registers.getRegister(rs2) & 0x1F); // Extract the shift amount
+	            int sourceValue = registers.getRegister(rs1); // Value to be shifted
+	            int shiftedResult = sourceValue >>> shiftValue; // Perform logical right shift
+	            registers.writeToRegister(rd, shiftedResult); // Store the result in the destination register
+	            break;
+	            
+            case InstructionSet.SRA:
+                int sraShiftValue = (registers.getRegister(rs2) & 0x1F); // Extract the shift amount
+                int sraSourceValue = registers.getRegister(rs1); // Value to be shifted
+                int sraResult = sraSourceValue >> sraShiftValue; // Perform arithmetic right shift
+                registers.writeToRegister(rd, sraResult); // Store the result in the destination register
+                break;
+                
+	        case InstructionSet.OR:
+	            int orFirstValue = registers.getRegister(rs1); // First value for bitwise OR
+	            int orSecondValue = registers.getRegister(rs2); // Second value for bitwise OR
+	            int orResult = orFirstValue | orSecondValue; // Perform bitwise OR operation
+	            registers.writeToRegister(rd, orResult); // Store the result in the destination register
+	            break;
+	            
+	        case InstructionSet.AND:
+	            int andFirstValue = registers.getRegister(rs1); // First value for bitwise AND
+	            int andSecondValue = registers.getRegister(rs2); // Second value for bitwise AND
+	            int andResult = andFirstValue & andSecondValue; // Perform bitwise AND operation
+	            registers.writeToRegister(rd, andResult); // Store the result in the destination register
+	            break;
+	            
+            
 
             // Add cases for other R-Type instructions as needed
 
